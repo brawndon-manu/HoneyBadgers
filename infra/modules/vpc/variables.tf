9872695be91e4f61948484844bd1eccd01fc036b
brawndon-manu <120494280+brawndon-manu@users.noreply.github.com>
2025-11-01 14:27:51 -0700

    feat(terraform): add VPC module variables

---
+++ infra/modules/vpc/variables.tf
@@ -0,0 +1,14 @@
+﻿variable "vpc_cidr"       { type = string }
+variable "public_subnets" { type = list(string) }
+variable "private_subnets"{ type = list(string) }
+
+variable "project_name" { type = string }
+variable "env"          { type = string }
+variable "aws_region"   { type = string }
+
+locals {
+  common_tags = {
+    Project = var.project_name
+    Env     = var.env
+  }
+}
