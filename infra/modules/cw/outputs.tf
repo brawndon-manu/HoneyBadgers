c2edb2b4798e58dc6fc014d08f1afb2ab2581c39
brawndon-manu <120494280+brawndon-manu@users.noreply.github.com>
2025-12-01 13:40:50 -0800

    chore(cw): expose events/flowlogs log group outputs

--- infra/modules/cw/outputs.tf
+++ infra/modules/cw/outputs.tf
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿output "flowlogs_log_group_name" {
   description = "Name of the VPC Flow Logs CloudWatch Log Group."
   value       = aws_cloudwatch_log_group.flowlogs.name
@@ -17,3 +18,20 @@ output "events_log_group_arn" {
   description = "ARN of the events CloudWatch Log Group."
   value       = aws_cloudwatch_log_group.events.arn
 }
+=======
+output "events_log_group_name" {
+  value = aws_cloudwatch_log_group.events.name
+}
+
+output "events_log_group_arn" {
+  value = aws_cloudwatch_log_group.events.arn
+}
+
+output "flowlogs_log_group_name" {
+  value = aws_cloudwatch_log_group.flowlogs.name
+}
+
+output "flowlogs_log_group_arn" {
+  value = aws_cloudwatch_log_group.flowlogs.arn
+}
+>>>>>>> abec886 (chore(cw): expose events/flowlogs log group outputs)
